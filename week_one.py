97defaa236d1f54123a50a32452a42844c0c339b
JacobMennell <jacobmennell@rockborne.com>
2022-05-03 15:27:30 +0100

    first commit, create week one file

--- week_one.py
+++ week_one.py
@@ -1,7 +1,11 @@
+
+######################################################################################################################
+
 class Point:
     """ Point class for representing and manipulating x,y coordinates. """
 
     def __init__(self, initX, initY):
+
         self.x = initX
         self.y = initY
 
@@ -11,7 +15,141 @@ class Point:
     def getY(self):
         return self.y
 
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
-point1 = Point(10,100)
 
-print(point1.getX(),point1.getY())
+p = Point(7,6)
+print(p.getX())
+print(p.getY())
+print(p.distanceFromOrigin())
+
+######################################################################################################################
+
+class Animal:
+    """ Animal class """
+
+    def __init__(self, arms, legs):
+
+        self.arms = arms
+        self.legs = legs
+
+    def limbs(self):
+        return self.arms + self.legs
+
+spider = Animal(4,4)
+spidlimbs = spider.limbs()
+print(spidlimbs)
+
+######################################################################################################################
+
+#call function on instance of the class. Note not a method of the class Point.
+
+import math
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+def distance(point1, point2):
+    xdiff = point2.getX()-point1.getX()
+    ydiff = point2.getY()-point1.getY()
+
+    dist = math.sqrt(xdiff**2 + ydiff**2)
+    return dist
+
+p = Point(4,3)
+q = Point(0,0)
+print(distance(p,q))
+
+######################################################################################################################
+
+# Alternative way passing distance as method
+import math
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def distance(self, point2):
+        xdiff = point2.getX()-self.getX()
+        ydiff = point2.getY()-self.getY()
+
+        dist = math.sqrt(xdiff**2 + ydiff**2)
+        return dist
+
+p = Point(4,3)
+q = Point(0,0)
+print(p.distance(q))
+
+######################################################################################################################
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return "x = {}, y = {}".format(self.x, self.y)
+
+p = Point(7,6)
+print(p)
+
+######################################################################################################################
+
+class Cereal:
+    """Cereal Class for brans of cereals """
+
+    def __init__(self, n, b, f):
+        self.name = n
+        self.brand = b
+        self.fiber = f
+
+    def __str__(self):
+        return f'{self.name} cereal is produced by {self.brand} and has {self.fiber} grams of fiber in every serving!'
+
+
+c1 = Cereal("Corn Flakes", "Kellogg's", 2)
+c2 = Cereal("Honey Nut Cheerios", "General Mills", 3)
+
+print(c1, c2)
+
+######################################################################################################################
