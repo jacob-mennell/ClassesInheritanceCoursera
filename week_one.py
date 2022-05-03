439f57c438c1cbc273568ab4b59aa15418369df0
JacobMennell <jacobmennell@rockborne.com>
2022-05-03 16:20:30 +0100

    end of day commit. Finished week 1 exercises.

--- week_one.py
+++ week_one.py
@@ -153,3 +153,111 @@ c2 = Cereal("Honey Nut Cheerios", "General Mills", 3)
 print(c1, c2)
 
 ######################################################################################################################
+
+class Point:
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def distanceFromOrigin(self):
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
+
+    def __str__(self):
+        return "x = {}, y = {}".format(self.x, self.y)
+
+    def halfway(self, target):
+        mx = (self.x + target.x)/2
+        my = (self.y + target.y)/2
+        return Point(mx, my)
+
+p = Point(3,4)
+q = Point(5,12)
+mid = p.halfway(q)
+# note that you would have exactly the same result if you instead wrote
+# mid = q.halfway(p)
+# because they are both Point objects, and the middle is the same no matter what
+
+print(mid)
+print(mid.getX())
+print(mid.getY())
+
+######################################################################################################################
+
+L = ["Cherry", "Apple", "Blueberry"]
+
+print(sorted(L, key=len))
+#alternative form using lambda, if you find that easier to understand
+print(sorted(L, key= lambda x: len(x)))
+
+class Fruit():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+L = [Fruit("Cherry", 10), Fruit("Apple", 5), Fruit("Blueberry", 20)]
+for f in sorted(L, key=lambda x: x.price):
+    print(f.name)
+
+class Fruit():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def sort_priority(self):
+        return self.price
+
+L = [Fruit("Cherry", 10), Fruit("Apple", 5), Fruit("Blueberry", 20)]
+print("-----sorted by price, referencing a class method-----")
+for f in sorted(L, key=Fruit.sort_priority):
+    print(f.name)
+
+print("---- one more way to do the same thing-----")
+for f in sorted(L, key=lambda x: x.sort_priority()):
+    print(f.name)
+
+
+######################################################################################################################
+
+class Point:
+    """ Point class for representing and manipulating x,y coordinates. """
+
+    printed_rep = "*"
+
+    def __init__(self, initX, initY):
+
+        self.x = initX
+        self.y = initY
+
+    def graph(self):
+        rows = []
+        size = max(int(self.x), int(self.y)) + 2
+        for j in range(size-1) :
+            if (j+1) == int(self.y):
+                special_row = str((j+1) % 10) + (" "*(int(self.x) -1)) + self.printed_rep
+                rows.append(special_row)
+            else:
+                rows.append(str((j+1) % 10))
+        rows.reverse()  # put higher values of y first
+        x_axis = ""
+        for i in range(size):
+            x_axis += str(i % 10)
+        rows.append(x_axis)
+
+        return "\n".join(rows)
+
+
+p1 = Point(2, 3)
+p2 = Point(3, 12)
+print(p1.graph())
+print()
+print(p2.graph())
+
+######################################################################################################################
