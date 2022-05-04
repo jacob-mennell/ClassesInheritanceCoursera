6096e00e35dc0a0dacc3d198bc8bb27e756e8ae2
JacobMennell <jacobmennell@rockborne.com>
2022-05-04 18:25:13 +0100

    end of day commit. Finished week 2 exercises.

--- week_one.py
+++ week_one.py
@@ -261,3 +261,47 @@ print()
 print(p2.graph())
 
 ######################################################################################################################
+#               ASSIGNMENT 1
+
+class Bike:
+    """ Class that represents bike parameters"""
+
+    def __init__(self, color, price):
+        self.color = color
+        self.price = price
+
+
+testOne = Bike('blue', 89.99)
+testTwo = Bike('purple', 25.0)
+
+
+class AppleBasket:
+    """ Information on Apples"""
+
+    def __init__(self, apple_color, apple_quantity):
+        self.apple_color = apple_color
+        self.apple_quantity = apple_quantity
+
+    def increase(self):
+        self.apple_quantity += 1
+
+    def __str__(self):
+        return f"A basket of {self.apple_quantity} {self.apple_color} apples."
+
+
+testOne = AppleBasket('blue', 4)
+print(testOne)
+testOne.increase()
+print(testOne)
+
+class BankAccount:
+    """ Takes name and money in account"""
+
+    def __init__(self, name, amt):
+        self.name = name
+        self.amt = amt
+
+    def __str__(self):
+        return f"Your account, {self.name}, has {self.amt} dollars."
+
+t1 = BankAccount('Bob', 100)
